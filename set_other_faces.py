446e0e44a5d88248c7b5672ac78742ebcb0cf835
Cosmos-UESTC <Xinyuefeng_UESTC@outlook.com>
2021-09-04 19:47:55 +0800

    update files

--- set_other_faces.py
+++ set_other_faces.py
@@ -27,20 +27,20 @@ for (path, dirnames, filenames) in os.walk(input_dir):
             # 使用detector进行人脸检测 dets为返回的结果
             dets = detector(gray_img, 1)
 
-            #使用enumerate 函数遍历序列中的元素以及它们的下标
-            #下标i即为人脸序号
-            #left：人脸左边距离图片左边界的距离 ；right：人脸右边距离图片左边界的距离 
-            #top：人脸上边距离图片上边界的距离 ；bottom：人脸下边距离图片上边界的距离
+            # 使用enumerate 函数遍历序列中的元素以及它们的下标
+            # 下标i即为人脸序号
+            # left：人脸左边距离图片左边界的距离 ；right：人脸右边距离图片左边界的距离
+            # top：人脸上边距离图片上边界的距离 ；bottom：人脸下边距离图片上边界的距离
             for i, d in enumerate(dets):
                 x1 = d.top() if d.top() > 0 else 0
                 y1 = d.bottom() if d.bottom() > 0 else 0
                 x2 = d.left() if d.left() > 0 else 0
                 y2 = d.right() if d.right() > 0 else 0
                 # img[y:y+h,x:x+w]
-                face = img[x1:y1,x2:y2]
+                face = img[x1:y1, x2:y2]
                 # 调整图片的尺寸
-                face = cv2.resize(face, (size,size))
-                cv2.imshow('image',face)
+                face = cv2.resize(face, (size, size))
+                cv2.imshow('image', face)
                 # 保存图片
                 cv2.imwrite(output_dir+'/'+str(index)+'.jpg', face)
                 index += 1
